e2e3e084cf2bc295f0a07d6218293d74e827d769
rzm0572 <1271920576@qq.com>
2020-01-23 15:40:20 +0800

    Main class completed

---
+++ src/main/java/com/github/rzm0572/useful/Useful.java
@@ -0,0 +1,38 @@
+package com.github.rzm0572.useful;
+
+import com.github.rzm0572.useful.common.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = "1.12.2")
+public class Useful {
+    @Instance
+    public static Useful instance;
+
+    @SidedProxy(serverSide = Reference.COMMON, clientSide = Reference.CLIENT)
+    public static CommonProxy proxy;
+
+    public static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+}
---
+++ src/main/java/com/github/rzm0572/useful/common/CommonProxy.java
@@ -0,0 +1,19 @@
+package com.github.rzm0572.useful.common;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+        // TODO
+    }
+
+    public void init(FMLInitializationEvent event) {
+        // TODO
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+        // TODO
+    }
+}
